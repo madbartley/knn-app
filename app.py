5719c2a1f43708ad3c316e2efe26850953c35666
Madeline Bartley <he9169@wayne.edu>
2025-06-11 16:16:30 -0400

    Finished the first iteration! Logic works, page looks polished, need to revise script in some areas

--- app.py
+++ app.py
@@ -41,6 +41,11 @@ def predict():
         y = data['diagnosis']
         random_set = random.randrange(0, 13, 1)
         input_chars = X.loc[random_set, :]
+        y_diagnosis = y[random_set]
+        if(y_diagnosis == 0):
+            y_label = "benign"
+        else:
+            y_label = "malignant"
     features = [np.array(input_chars)]
     scaler = load('std_scaler.bin')
     features_std = scaler.transform(features)
@@ -50,7 +55,7 @@ def predict():
     else:
         diagnosis = 'malignant'
 
-    return render_template('welcome.html', output='This tumor is likely to be {}'.format(diagnosis))
+    return render_template('welcome.html', output='This tumor is likely to be {}'.format(diagnosis), zero=' {}'.format(input_chars[0]), one=' {}'.format(input_chars[1]), two=' {}'.format(input_chars[2]), three=' {}'.format(input_chars[3]), four=' {}'.format(input_chars[4]), five=' {}'.format(input_chars[5]), six=' {}'.format(input_chars[6]), seven=' {}'.format(input_chars[7]), eight=' {}'.format(input_chars[8]), nine=' {}'.format(input_chars[9]), ten=' {}'.format(input_chars[10]), eleven=' {}'.format(input_chars[11]), twelve=' {}'.format(input_chars[12]), thirteen=' {}'.format(input_chars[13]), fourteen=' {}'.format(input_chars[14]), fifteen=' {}'.format(input_chars[15]), sixteen=' {}'.format(input_chars[16]), seventeen=' {}'.format(input_chars[17]), eighteen=' {}'.format(input_chars[18]), nineteen=' {}'.format(input_chars[19]), twenty=' {}'.format(input_chars[20]), twentyone=' {}'.format(input_chars[21]), twentytwo=' {}'.format(input_chars[22]), twentythree=' {}'.format(input_chars[23]), twentyfour=' {}'.format(input_chars[24]), twentyfive=' {}'.format(input_chars[25]), twentysix=' {}'.format(input_chars[26]), twentyseven=' {}'.format(input_chars[27]), twentyeight=' {}'.format(input_chars[28]), twentynine=' {}'.format(input_chars[29]), label=' {}'.format(y_label))
 
 if __name__ == "__main__":
     app.run()
